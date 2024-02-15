b3364342f94fb8fbc19aa795f028b86e7cf31490
ramonmelod <ramonmelo.com@gmail.com>
2024-02-15 19:53:27 -0300

    agrupamento arquivos de infra e implementação inicial de post e get no index.js

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-function a() {
-  const a = 5;
-}
+const express = require("express");
+const { Router } = require("express");
+const db = require("./infra/database");
+const porta = 3000;
+const localhost = "localhost";
+
+const app = express();
+
+app.post("/post", (req, res) => {
+  res.type("text/plain");
+  console.log("endpoit post");
+  res.send("Pagina post");
+});
+
+app.get("/", (req, res) => {
+  res.type("text/plain");
+  console.log("endpoit get");
+  res.send("Pagina get");
+});
+
+app.listen(porta, localhost, () => {
+  console.log("servidor rodando!");
+});
