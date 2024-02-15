802bbdbf8b017481f4286c61b23aa12ac5303c27
ramonmelod <ramonmelo.com@gmail.com>
2024-02-15 20:50:21 -0300

    implementação da captura de paramentros da URL

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const express = require("express");
 const { Router } = require("express");
 const db = require("./infra/database");
+const app = express();
+const apiRouter = Router();
 const porta = 3000;
 const localhost = "localhost";
 
-const app = express();
+//app.use(express.json());
 
-app.post("/post", (req, res) => {
+app.use("/clientes", apiRouter);
+//app.use("/clientes/:id/extrato", apiRouter);
+
+apiRouter.post("/:id/transacoes", (req, res) => {
+  const id = req.params.id;
   res.type("text/plain");
+  /*   const A = "1";
+  const B = "2"; */
   console.log("endpoit post");
-  res.send("Pagina post");
+  res.send("seu id é: " + id);
 });
 
-app.get("/", (req, res) => {
+/* apiRouter.get("/clientes/[id]/extrato", (req, res) => {
   res.type("text/plain");
   console.log("endpoit get");
   res.send("Pagina get");
-});
+}); */
 
 app.listen(porta, localhost, () => {
   console.log("servidor rodando!");
