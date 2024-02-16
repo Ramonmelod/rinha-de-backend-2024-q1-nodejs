f0e670ee5555f4fccb7831cc2ed29c93a8040f14
ramonmelod <ramonmelo.com@gmail.com>
2024-02-15 22:32:38 -0300

    implementação do arquivo init.sql e inserção de instruções para inicio do container postgres no README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rinha-de-backend-2024-q1-nodejs
+
 Implementação de API em express para a rinha de backend 2024
+
+- tecnologias utilizadas: Express, Postgres
+  inicialização do container: docker compose -f infra/compose.yaml up -d
--- index.js
+++ index.js
@@ -8,25 +8,26 @@ const localhost = "localhost";
 
 //app.use(express.json());
 
-app.use("/clientes", apiRouter);
-//app.use("/clientes/:id/extrato", apiRouter);
+app.use("/clientes", apiRouter); // router que permite captura de parâmetros da URL para o endPoint /clientes
 
 apiRouter.post("/:id/transacoes", (req, res) => {
   const id = req.params.id;
   res.type("text/plain");
-  /*   const A = "1";
-  const B = "2"; */
   console.log("endpoit post");
-  res.send("Seu id via post é: " + id);
+  res.status(200).send("Seu id via post é: " + id);
 });
 
 apiRouter.get("/:id/extrato", (req, res) => {
   res.type("text/plain");
   const id = req.params.id;
   console.log("endpoit get");
-  res.send("Seu id via get é: " + id);
+  res.status(200).send("Seu id via get é: " + id);
 });
 
 app.listen(porta, localhost, () => {
   console.log("servidor rodando!");
 });
+
+/*notas:
+os serviços não devem demorar mais que 40 segundos para subir estar disponiveis
+*/
