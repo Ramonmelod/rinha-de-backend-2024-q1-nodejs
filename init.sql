4e5e6aa3016f5f5d39f2f8980c8f4ab68f66394a
ramonmelod <ramonmelo.com@gmail.com>
2024-02-18 13:11:38 -0300

    implementação da inserção de dados na tabela clientes

--- init.sql
+++ init.sql
@@ -2,7 +2,7 @@ CREATE TABLE clientes(
   i_id_clientes SERIAL PRIMARY KEY,
   s_nome_clientes varchar(100) NOT NULL,
   s_limite_clientes INT NOT NULL,
-  s_saldo_clientes INT NOT NULL
+  s_saldo_clientes INT NOT NULL DEFAULT 0
 );
 
 CREATE TABLE transacoes(
@@ -11,6 +11,15 @@ CREATE TABLE transacoes(
   s_tipo_transacoes  CHAR(1) NOT NULL,
   s_descricao_transacoes   VARCHAR(100) NOT NULL,
   s_realizada_transacoes  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+);
+
+INSERT INTO clientes (s_nome_clientes, s_limite_clientes)
+VALUES
+  ('o barato sai caro', 1000 * 100),
+  ('zan corp ltda', 800 * 100),
+  ('les cruders', 10000 * 100),
+  ('padaria joia de cocaia', 100000 * 100),
+  ('kid mais', 5000 * 100);
+
 
 /*NÃO ESQUECER DE GARANTIR QUE CLIENTE 6 NÃO SEJA CRIADO*/
