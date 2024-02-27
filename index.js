47cb599c8f9fef5af7f66ac384c5adba260a7cdf
Ramon <ramonmelo.com@gmail.com>
2024-02-27 08:19:14 -0300

    refatoração endpoint /clientes/[id]/transacoes

--- index.js
+++ index.js
@@ -14,49 +14,35 @@ apiRouter.post("/:id/transacoes", async (req, res) => {
   console.log("endpoit post");
   res.type("application/json");
   const id = req.params.id;
-  const { valor } = req.body;
+  let { valor } = req.body;
   const { tipo } = req.body;
   const { descricao } = req.body;
   if (tipo === "c") {
     // implementar controle de limite de saldo
     // crédito de valor em conta
-    const query1 = await db.query({
-      text: "UPDATE clientes SET s_saldo_clientes = s_saldo_clientes + $1 WHERE i_id_clientes = $2",
-      values: [valor, id],
-    });
-    const query2 = await db.query({
-      text: "INSERT INTO transacoes(i_valor_transacoes, i_id_cliente, s_tipo_transacoes, s_descricao_transacoes) values($1,$2,$3,$4)",
-      values: [valor, id, tipo, descricao],
-    });
-
-    const query3 = await db.query({
-      // consulta limite e saldo de acordo com o id vindo na url
-      text: "SELECT s_limite_clientes, s_saldo_clientes FROM clientes WHERE i_id_clientes = $1",
-      values: [id],
-    });
-    const resultadoConsulta = parseInt(query3); // verificar necessidade do parse
-    res.status(200).send(query3.rows);
+    valor = valor;
   } else if (tipo === "d") {
-    // crédito de valor em conta
-    const query1 = await db.query({
-      text: "UPDATE clientes SET s_saldo_clientes = s_saldo_clientes - $1 WHERE i_id_clientes = $2", // nesta linha é feita a subtração
-      values: [valor, id],
-    });
-    const query2 = await db.query({
-      text: "INSERT INTO transacoes(i_valor_transacoes, i_id_cliente, s_tipo_transacoes, s_descricao_transacoes) values($1,$2,$3,$4)",
-      values: [valor, id, tipo, descricao],
-    });
-
-    const query3 = await db.query({
-      // consulta limite e saldo de acordo com o id vindo na url
-      text: "SELECT s_limite_clientes, s_saldo_clientes FROM clientes WHERE i_id_clientes = $1",
-      values: [id],
-    });
-    const resultadoConsulta = parseInt(query3); // verificar necessidade do parse
-    res.status(200).send(query3.rows);
+    // débito de valor em conta
+    valor = -valor;
   } else {
     res.status(422).send("operação não existente para tipo =  " + tipo);
   }
+  const query1 = await db.query({
+    text: "UPDATE clientes SET s_saldo_clientes = s_saldo_clientes + $1 WHERE i_id_clientes = $2",
+    values: [valor, id],
+  });
+  const query2 = await db.query({
+    text: "INSERT INTO transacoes(i_valor_transacoes, i_id_cliente, s_tipo_transacoes, s_descricao_transacoes) values($1,$2,$3,$4)",
+    values: [valor, id, tipo, descricao],
+  });
+
+  const query3 = await db.query({
+    // consulta limite e saldo de acordo com o id vindo na url
+    text: "SELECT s_limite_clientes, s_saldo_clientes FROM clientes WHERE i_id_clientes = $1",
+    values: [id],
+  });
+  const resultadoConsulta = parseInt(query3); // verificar necessidade do parse
+  res.status(200).send(query3.rows);
 });
 
 apiRouter.get("/:id/extrato", async (req, res) => {
